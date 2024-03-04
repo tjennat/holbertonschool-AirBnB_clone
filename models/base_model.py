ff264e3270085a209bc81519bb7cda6b0f136631
tjennat <theo.jennat98@gmail.com>
2024-03-04 21:17:31 +0100

    recreating an instance using args and kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -7,12 +7,18 @@ from datetime import datetime
 
 class BaseModel:
     """This is the BaseModel class for the AirBnB clone"""
-
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """__init__ method"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.utcnow()
-        self.updated_at = datetime.utcnow()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+                elif key != "__class__":
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.utcnow()
+            self.updated_at = datetime.utcnow()
 
     def __str__(self):
         """__str__ method"""
