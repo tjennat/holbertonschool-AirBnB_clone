d24183b8d87aed391665473e07c05bb6add6a9f6
tjennat <theo.jennat98@gmail.com>
2024-03-07 12:40:31 +0100

    erasing help function because it's provided by default

--- console.py
+++ console.py
@@ -5,7 +5,6 @@ from models.base_model import BaseModel
 
 
 class HBNBCommand(cmd.Cmd):
-    """class for the console"""
     prompt = "(hbnb) "
 
     def do_quit(self, line):
@@ -14,16 +13,7 @@ class HBNBCommand(cmd.Cmd):
 
     def do_EOF(self, line):
         """EOF command"""
-        print()
         return True
-
-    def help_quit(self):
-        """help quit command"""
-        print("Quit command to exit the program")
-
-    def help_EOF(self):
-        """help EOF command"""
-        print("EOF command to exit the program")
 
     def emptyline(self):
         """empty line"""
