cd4863b8176e70ac0036117934df9c252c0774d2
tjennat <theo.jennat98@gmail.com>
2024-03-08 17:16:51 +0100

    classes that inherits from base model

---
+++ models/city.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This is going to be my class for city"""
+
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """This is the City class"""
+    state_id = ""
+    name = ""
---
+++ models/state.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This is going to be my class for state"""
+
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """This is the State class"""
+    name = ""
